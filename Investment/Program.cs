02b2dd7664a8cdf3b8c298d925fd8f6c1af3fa8d
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-08 23:31:56 +0530

    Add files via upload

---
+++ Investment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args){
+        Console.WriteLine("Welcome Investor");
+        if(args.Length > 1){
+            double p = double.Parse(args[0]);
+            int n = int.Parse(args[1]);
+
+            Console.WriteLine("Future value of risk free Investment is {0:0.00}",Investment.FutureValue(p,n,false));
+
+            Console.WriteLine("Future value of risk-full Investment is {0:0.00}",Investment.FutureValue(p,n,true));
+
+        }
+    }
+}
