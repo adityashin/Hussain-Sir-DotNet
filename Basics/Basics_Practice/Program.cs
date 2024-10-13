2835cfcbf93dde5afc2eb27bd13709243b7d6dd1
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-13 23:52:49 +0530

    Add files via upload

---
+++ Basics/Basics_Practice/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+    //1
+    float number = 369.369f;
+    Console.WriteLine(number);
+
+    //2
+    
+        double num = 10;
+        string oe = num%2==0 ? "even" : "odd";
+        Console.WriteLine(oe);
+
+    //3
+    
+        int no=1;
+        while(no<=10)
+        {
+            Console.WriteLine(no);
+            ++no;
+        }
+
+    //4
+    const double pi = 3.14;
+    Console.WriteLine(pi);
+
+    //5
+    int day = 6;
+    switch(day){
+        case 1:
+            Console.WriteLine("Sunday");
+            break;
+        case 2:
+            Console.WriteLine("Monday");
+            break;
+        case 3:
+            Console.WriteLine("Tuesday");
+            break;
+        case 4:
+            Console.WriteLine("Wednesday");
+            break;
+        case 5:
+            Console.WriteLine("Thursday");
+            break;
+        case 6:
+            Console.WriteLine("Friday");
+            break;
+        case 7:
+            Console.WriteLine("Saturday");
+            break;
+        default:
+            Console.WriteLine("Invalid Number");
+            break;
+    }
+
+    }
+
+}
---
+++ Basics/Methods_practice/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Program
+{
+    //1
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    // //2
+    static int PrintSquare(int n)
+    {
+        int i=0;
+        while(i<=n)
+        {
+            Console.WriteLine(i*i);
+            ++i;
+        }
+        return 0;
+    }
+
+    // //3
+    //palindrom
+
+    //4
+     static int ArraySum(int[] arr)
+     {
+        int num=0;
+        int length = arr.Length;
+        for(int i=0;i<=length;++i)
+        {
+            if(num <= arr[i])
+                num = arr[i];
+        }
+        Console.WriteLine($"Biggest number in array is {num}");
+        return 0;
+
+     }
+
+     //factorial
+     static int factorial(int num)
+     {
+        int fact = 1;
+        for(int i=1;i<=num;++i)
+        {
+            fact*=i;
+        }
+        Console.WriteLine($"Factorail of {num} is {fact}");
+        return 0;
+     }
+    public static void Main(string[] args)
+    {
+        int addno1 = int.Parse(args[0]);
+        int addno2 = int.Parse(args[1]);
+        Console.WriteLine("Addition of two numbers is {0}", Add(addno1, addno2));
+
+        int sqno = int.Parse(args[2]);
+        PrintSquare(sqno);
+
+        // int[] arrray = {1,2,3,4,5};
+        // ArraySum(arrray);
+
+        factorial(14);
+
+
+
+    }
+}
