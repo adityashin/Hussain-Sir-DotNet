5b3a901f59cb041aac2d7ad8a9f19c031e2bbb6d
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-16 23:38:41 +0530

    Update Program.cs

--- Basics/Inheritance_practice/Program.cs
+++ Basics/Inheritance_practice/Program.cs
@@ -1,9 +1,29 @@
-﻿using Transport;
+using System;
+
+class Vehicle
+{
+    public void displayVehicle()
+    {
+        Console.WriteLine($"Display of Vehicle");
+    }
+}
+
+class Bicycle : Vehicle
+{
+    private string price { get; set; }
+
+    public void displayBicycle()
+    {
+        Console.WriteLine($"Display of Bicycle");
+    }
+}
 
 class Program
 {
     public static void Main(string[] args)
     {
-
+        Bicycle b = new Bicycle();
+        b.displayBicycle();
+        b.displayVehicle();
     }
 }
