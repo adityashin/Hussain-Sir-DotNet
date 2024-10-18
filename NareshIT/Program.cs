8c4eb6e8f8f05710578ed25b4dfa35bfd2165aaa
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-18 23:46:58 +0530

    Add files via upload

---
+++ NareshIT/Program.cs
@@ -0,0 +1,41 @@
+﻿class A
+{
+
+    public A(int i)
+    {
+        Console.WriteLine("Hello A's Here {0}",i);
+
+    }
+}
+class B: A
+{
+   public B(int i):base(i)
+    {
+        Console.WriteLine("Hello B's here {0}",i);
+
+    }
+}
+// class C:B
+// {
+//     public C()
+//     {
+//         Console.WriteLine("Hello C's here");
+//     }
+// }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        A aadi = new A(10);
+        B bbb = new B(10);
+        // C ccc = new C();
+
+        Console.WriteLine(aadi);
+        Console.WriteLine(bbb);
+        // Console.WriteLine(ccc);
+
+        // Console.WriteLine(aadi.GetType);
+        // Console.WriteLine(bbb.GetType);
+        // Console.WriteLine(ccc.GetType);
+    }
+}
