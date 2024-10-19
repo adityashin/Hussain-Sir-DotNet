202baa52fa88d47a129ba56c6cb22bd47fa0a346
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-19 21:08:01 +0530

    Update Program.cs

--- NareshIT/Inheritance Practical/Program.cs
+++ NareshIT/Inheritance Practical/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿using School;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Student s = new Student("01","aditya","solapur","Indian","CDAC","100%","A+","100000");
+        s.DisplayInfo();
+
+        TeachingStaff t = new TeachingStaff("01","Hussain","NYC","American","PHD in Quantam Physics","Every subject in Computer Science");
+        t.DisplayInfo();
+    }
+}
