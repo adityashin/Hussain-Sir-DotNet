71436c9fa304b0c05fc14a9968d714752c915c35
Aditya Anand Shinde <71961975+adityashin@users.noreply.github.com>
2024-10-19 21:06:31 +0530

    Update Person.cs

--- NareshIT/Inheritance Practical/School/Person.cs
+++ NareshIT/Inheritance Practical/School/Person.cs
@@ -1,1 +1,22 @@
-namespace 
+using System.IO.Pipes;
+
+namespace School
+{
+    public abstract class Person
+    {
+        public string id {get; set;}
+        public string name {get; set;}
+        public string address {get; set;}
+        public string phone {get; set;}
+
+        public Person(string id,string name,string address,string phone)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public abstract void DisplayInfo();
+    }
+}
